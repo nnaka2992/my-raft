c17da8f7dd452f53773615d6e5dad3887f5bcc82
nnaka2992 <nnaka2992@gmail.com>
2023-12-31 03:34:49 +0900

    add cli flags

--- main.go
+++ main.go
@@ -1,6 +1,9 @@
 package main
 
 import (
+	"os"
+	"flag"
+	"fmt"
 	"net/http"
 
 	"github.com/labstack/echo/v4"
@@ -8,8 +11,33 @@ import (
 )
 
 var s *StateMachine
+var (
+	sState = flag.String("state", "LEADER", "state of the node	(LEADER, FOLLOWER)")
+	sAddr = flag.String("addr", "localhost", "address of the node")
+	sPort = flag.String("port", "8080", "port of the node")
+	bHelp = flag.Bool("help", false, "show this message")
+	bHelpShort = flag.Bool("h", false, "show this message")
+)
+
 func main() {
-	s = NewNode(LEADER, "localhost:8080")
+	flag.Usage = func() {
+		fmt.Fprintf(os.Stderr, "Usage: %s [<state> <addr> <port>]\n", os.Args[0])
+		flag.PrintDefaults()
+	}
+	flag.Parse()
+	if *bHelp || *bHelpShort {
+		flag.Usage()
+		os.Exit(0)
+	}
+	switch *sState {
+	case "LEADER":
+		s = NewNode(LEADER, *sAddr + ":" + *sPort)
+	case "FOLLOWER":
+		s = NewNode(FOLLOWER, *sAddr + ":" + *sPort)
+	default:
+		flag.Usage()
+		os.Exit(0)
+	}
 
 	e := echo.New()
 	e.Use(middleware.Logger())
@@ -18,7 +46,7 @@ func main() {
 	//	e.POST("/raft/heartbeat", PostReceiveHeartbeat)
 	e.GET("/raft/health", GetHealth)
 	e.POST("/raft/follower/new", PostNewFollower)
-	e.Logger.Fatal(e.Start(":8080"))
+	e.Logger.Fatal(e.Start(fmt.Sprintf(":%s", *sPort)))
 }
 
 func GetHealth(c echo.Context) error {
